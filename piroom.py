2639acb203c7c4e1c5a2451baba57d3fa7e8ed8f
ekremcet <ekrem.cetinkaya@ozu.edu.tr>
2017-05-01 12:43:08 +0000

    Updated status

--- piroom.py
+++ piroom.py
@@ -3,47 +3,54 @@ from firebase import firebase
 from time import sleep
 
 #Firebase settings, Initialize room info
-room_info = {'Name':'TEST', 'Status' : 0}
 DB_secret = 'q7bAYVPdP3JhFZdWozGxmHkaefaseutKrdJn3pKR'
 authentication = firebase.FirebaseAuthentication(DB_secret,'piroom.ozu@gmail.com',True,True)
 firebase = firebase.FirebaseApplication("https://piroom-f14c0.firebaseio.com/rooms", authentication)
-firebase.put('/rooms/1',1,room_info)
 
-def check_movement(room_status):
+#Status
+room_status = 0
+empty_count = 0
+
+
+
+
+def check_movement(empty_count):
+    temp_count = empty_count + 1
     print("Checking movement")
     #Check room for certain seconds
     #If you detect a movement mark it room as full
-    seconds = 50
-    room_status = False
+    seconds = 50 # seconds * 10
     for _ in range (seconds):
         if pir.motion_detected:
             print("Movement")
-            room_status = True
+            temp_count = 0
             break
         sleep(0.1)
     
-    return room_status
-
-def update_database(room_status):
-    status = 0
-    if room_status:
-        status = 1
-    #rooms/b1/kat1 --> Update status   
-    firebase.put("/rooms/1/1","/Status",status)
+    return temp_count
     
+def update_database(empty_count):
+    room_status = 2
+    if empty_count >= 6:
+        room_status = 1
+    if empty_count >= 12:
+        room_status = 0
+    #rooms/509A --> Update status   
+    firebase.put("/rooms/509A","/status",room_status)
+        
 pir = MotionSensor(4) # Place of motion sensor OUT cable
-control_frequency = 5 # Check movement at every 5 seconds
+control_frequency = 1 # Check movement at every 5 seconds
 
 print ("Getting Ready..")
 sleep(2)
 print ("Ready to detect")
 
-room_status = False
-
 try:
     while True:
-        room_status = check_movement(room_status)
-        update_database(room_status)
+        empty_count = check_movement(empty_count)
+        update_database(empty_count)
+        print("Empty Count : %i" %empty_count)
+        print("Cooldown")
         sleep(control_frequency)
     
 except KeyboardInterrupt:
