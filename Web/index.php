1b53927d6948885955ebbd30ee5d881d6a1b0889
erdenbatuhan <erden.batuhan@gmail.com>
2017-05-30 02:25:39 +0300

    Added buttons for each building

--- Web/index.php
+++ Web/index.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+
+include "./Model/School.php";
+include "./Model/Building.php";
+include "./Model/Room.php";
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,22 +21,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- ========== CSS Part ========== !-->
-    <link rel="stylesheet" type="text/css" href="inc/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="inc/css/styleutiful.css">
+    <link rel="stylesheet" type="text/css" href="Data/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="Data/css/styleutiful.css">
     <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Cookie">
 
     <!-- ========== JavaScript Part ========== !-->
-    <script src="inc/js/bootstrap.js"></script>
+    <script src="Data/js/bootstrap.js"></script>
 </head>
 <body>
-    <?php include "navbar.php" ?>
+    <?php include "navbar.php"; ?>
 
-    <div class="container text-center">
-        <br><br><br><br>
-        <h4> This site will be constructed soon. </h4>
+    <div id="main" class="container text-center">
+        <br><br><br><br><br>
+        <button type="button" class="btn btn-primary"> EF <br><i> Engineering Faculty </i></button>
+        <br><br>
+        <button type="button" class="btn btn-success"> FEAS <br><i> Faculty of Economics and Administrative Sciences </i></button>
+        <br><br>
+        <button type="button" class="btn btn-info"> LF <br><i> Law Faculty </i></button>
+        <br><br>
+        <button type="button" class="btn btn-warning"> ScOLa <br><i> School Of Languages </i></button>
+        <br><br>
+        <button type="button" class="btn btn-danger"> IC <br><i> Innovation Center </i></button>
+        <br><br>
     </div>
 
-    <?php include "footer.php" ?>
+    <script src="https://www.gstatic.com/firebasejs/4.0.0/firebase.js"></script>
+    <script>
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyBO50phc2VOZXASMumHxgSK6PGSMm4i4Pg",
+            authDomain: "piroom-f14c0.firebaseapp.com",
+            databaseURL: "https://piroom-f14c0.firebaseio.com",
+            projectId: "piroom-f14c0",
+            storageBucket: "piroom-f14c0.appspot.com",
+            messagingSenderId: "233603120330"
+        };
+
+        firebase.initializeApp(config);
+        var ref = firebase.database().ref('buildings');
+
+        //ref.on('value', function(snapshot) {
+            snapshot.forEach(function(child) {
+                var div = document.createElement('div');
+                div.className = "button";
+                div.innerHTML = "<button>" + child.val().abbreviation + "<hr><br>" + child.val().name + "</button>";
+
+                document.getElementById('main').appendChild(div);
+            });
+        });
+    </script>
+
+    <?php include "footer.php"; ?>
 </body>
 </html>
--- Web/navbar.php
+++ Web/navbar.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-light navbar-fixed-top bg-primary" style="border-radius: 0px; border-bottom-style: groove; border-bottom-color: black; border-bottom-width: 3px;">
     <div class="container-fluid">
-        <a class="navbar-brand" style="color: whitesmoke;" href="./"><h3>Pi<span>Room</span></h3></a>
+        <a class="navbar-brand" style="color: whitesmoke;" href="../"><h3>Pi<span>Room</span></h3></a>
+        <p class="navbar-text navbar-right"> Ozyegin University </p>
     </div>
 </nav>
