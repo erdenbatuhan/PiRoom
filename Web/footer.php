f4ab7dc7ab5bab3f27a9b135d127056c91f84b57
erdenbatuhan <erden.batuhan@gmail.com>
2017-05-31 23:40:16 +0300

    Developed Web

--- Web/footer.php
+++ Web/footer.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-light navbar-fixed-bottom bg-danger footer" style="border-radius: 0px; padding-top: 10px;">
         <div class="container text-center">
-            <p><i> &copy; 2015 PiRoom </i><p>
+            <p><i> &copy; 2017 PiRoom </i><p>
         </div>
 </nav>
